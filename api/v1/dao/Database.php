f6e24687f289860098880c269cdc9dba1cc10bee
Oriana <orianacmv@gmail.com>
2023-01-23 21:34:21 -0300

    Add: Database connection

---
+++ api/v1/dao/Database.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Api\V1\Dao;
+
+use PDO;
+
+class Database {
+
+  private $host;
+  private $user;
+  private $password;
+  private $dbName;
+  protected $connection;
+  private $stmt;
+
+  public function __construct()
+  {
+    $this->host = $_ENV['SERVER'];
+    $this->user = $_ENV['DB_USER'];
+    $this->password = $_ENV['DB_PASSWORD'];
+    $this->dbName = $_ENV['DB_NAME'];
+    $this->connect();
+  }
+
+  protected function connect(): void
+  {
+    try {
+      $dsn = 'mysql:host='. $this->host . ';dbname=' .$this->dbName;
+      $this->connection = new PDO($dsn, $this->user, $this->password);
+    } catch (\PDOException $e) {
+      throw new \PDOException($e->getMessage(), $e->getCode());
+    }
+  }
+
+  private function closeConnection(): void
+	{
+		$this->connection->stmt = null;
+		$this->connection = null;
+	}
+
+  public function  __destruct()
+	{
+		$this->closeConnection();
+	}
+}
